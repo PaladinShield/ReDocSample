5a1ba98f44903b5befccdaf50197f01fb2368ffb
joy777park <joy777park@gmail.com>
2023-05-09 23:30:33 +0800

    修改 redoc

--- ReDocSample/Controllers/WeatherForecastController.cs
+++ ReDocSample/Controllers/WeatherForecastController.cs
@@ -43,6 +43,7 @@ public class WeatherForecastController : ControllerBase
     OperationId = "Get",
     Tags = new[] { "WeatherForecast" })]
     [SwaggerResponse(200, "The random weather forecasts", typeof(WeatherForecast))]
+    [Produces("application/json")]
     public IEnumerable<WeatherForecast> Get([FromBody] WeatherForecastRequest request)
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
