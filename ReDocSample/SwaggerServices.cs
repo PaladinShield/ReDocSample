e82e5814a51a727aba296ccca67b3928854f8d41
weiyilai <joy777park@gmail.com>
2025-01-26 17:57:56 +0800

    update redoc

---
+++ ReDocSample/SwaggerServices.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace ReDocSample;
+
+public static class SwaggerServices
+{
+    /// <summary>
+    /// Add Swagger services to the application.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1",
+                new OpenApiInfo
+                {
+                    Title = "Swagger Demo Documentation",
+                    Version = "v1",
+                    Description = "This is a demo to see how documentation can easily be generated for ASP.NET Core Web APIs using Swagger and ReDoc.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "weiyi lai",
+                        Email = "joy777park@gmail.com"
+                    }
+                });
+
+            string xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            options.IncludeXmlComments(xmlPath);
+            options.EnableAnnotations();
+        });
+
+        return services;
+    }
+}
